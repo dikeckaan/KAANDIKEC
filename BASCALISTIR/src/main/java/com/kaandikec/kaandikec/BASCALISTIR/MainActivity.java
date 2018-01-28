b40ccf1850751bce2431b2a2f089c6780c565b01
KAAN DIKEC <dikeckaan@gmail.com>
2018-01-28 15:12:48 +0300

    deneme

---
+++ BASCALISTIR/src/main/java/com/kaandikec/kaandikec/BASCALISTIR/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kaandikec.kaandikec.BASCALISTIR;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
